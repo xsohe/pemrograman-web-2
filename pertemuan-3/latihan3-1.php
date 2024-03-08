00c124355043c65c0dac42990efc5135b7668d21
YanuarEka <yanuarekasusila@gmail.com>
2024-03-08 17:05:52 +0700

    feat: task meet 3

---
+++ pertemuan-3/latihan3-1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan pertemuan 3</title>
+</head>
+<body>
+    <center>
+        <form action="" method="post">
+            <input type="text" name="nilai1" placeholder="Nilai 1" required>
+            <select name="operator" id="operator">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input type="text" name="nilai2" placeholder="Nilai 2">
+            <input type="submit" name="submit" value="Submit" required>
+        </form>
+        </br>
+    </center>
+
+    <?php 
+        if(isset($_POST['submit'])) {
+            $operator = $_POST["operator"];
+            $nilai1 = $_POST['nilai1'];
+            $nilai2 = $_POST['nilai2'];
+
+            switch($operator) {
+                case "+":
+                    $hasil = $nilai1 + $nilai2;
+                    break;
+                case "-":
+                    $hasil = $nilai1 - $nilai2;
+                    break;
+                case "*":
+                    $hasil = $nilai1 * $nilai2;
+                    break;
+                case "/":
+                    $hasil = $nilai1 / $nilai2;
+                    break;
+                default: 
+                    echo "Masukan nilai terlebih dahulu!";
+            }
+
+            echo "<center><font size=5>Hasil : $hasil</font></center>";
+        }
+    ?>
+
+</body>
+</html>
